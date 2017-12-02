a4b1620eda4eb501a35b09f9345e139115a820fc
gabriel <777canaan@gmail.com>
2017-12-02 20:08:18 +1300

    make login mutation

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -1,7 +1,5 @@
-const graphql = require ('graphql')
-const {
-  GraphQLObjectType, GraphQLString
-} = graphql
+const graphql = require('graphql')
+const {GraphQLObjectType, GraphQLString} = graphql
 const UserType = require('./types/user_type')
 const AuthService = require('../services/auth')
 
@@ -14,16 +12,26 @@ const mutation = new GraphQLObjectType({
         email: { type: GraphQLString },
         password: { type: GraphQLString }
       },
-      resolve(parentValue, { email, password }, req) {
-        return AuthService.signup({ email, password, req })
+      resolve(parentValue, { email,password }, req) {
+        return AuthService.signup({email, password, req})
       }
     },
     logout: {
       type: UserType,
       resolve(parentValue, args, req) {
-        const { user } = req
+        const {user} = req
         req.logout()
         return user
+      }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email,password }, req) {
+        return AuthService.login({email, password, req })
       }
     }
   }
